85ddefdd9bf00568dd77806c1c7bd07b20e67c18
Alexandrdrdrd <kands12349@gmail.com>
2021-11-02 18:02:17 +0300

    added exceptions, changed logic

--- src/VenueService.groovy
+++ src/VenueService.groovy
@@ -7,43 +7,40 @@ class VenueService {
         foursquareAPIhttpClient = new FoursquareAPIhttpClient()
     }
 
-    public void find_nearest_venue(String[] coordinatesAndSection) {
-        JsonNode json_result = foursquareAPIhttpClient.findVenuesByDistance(coordinatesAndSection)
-        if (json_result.get("response").get("groups").get(0).get("items").size() == 0) {
-            println("There is no venues on current coordinates")
-            return
-        }
-        int min_distance = json_result.get("response").get("groups").get(0).get("items").get(0).get("venue").get("location").get("distance").asInt()
-
-        Map<String, Integer> result_map = new HashMap<>()
-        for (int i = 0; i < json_result.get("response").get("groups").get(0).get("items").size(); i++) {
-
-            if (min_distance == json_result.get("response").get("groups").get(0).get("items").get(i + 1).get("venue").get("location").get("distance").asInt()) {
-
-                json_result = foursquareAPIhttpClient.findVenuesByPopularity(coordinatesAndSection, min_distance)
-                result_map.put(json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("name").asText()
-                        , json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("location").get("distance").asInt())
-                break
-
-            } else {
-
-                result_map.put(json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("name").asText()
-                        , json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("location").get("distance").asInt())
-
-                break
+    public void find_nearest_venue(String[] coordinatesAndSection) throws IOException {
+        try{
+            JsonNode json_result = foursquareAPIhttpClient.findVenuesByDistance(coordinatesAndSection)
+            if (json_result.get("response").get("groups").get(0).get("items").size() == 0) {
+                println("There is no venues on current coordinates")
+                return
             }
+            int min_distance = json_result.get("response").get("groups").get(0).get("items").get(0).get("venue").get("location").get("distance").asInt()
+            def result_list = []
+
+            for (int i = 0; i<json_result.get("response").get("groups").get(0).get("items").size();i++){
+                Map<String, Integer> result_map = new HashMap<>()
+                result_map.put(json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("name").asText()
+                        , json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("location").get("distance").asInt())
+                result_list.add(result_map)}
+            print_result(min_distance, result_list)
+        }catch (IOException e){
+            println(e.getMessage())
+            println("Pleas, try again")
         }
 
-        print_result(min_distance, result_map)
+        }
 
-    }
 
-    private void print_result(int min_distance, Map<String, Integer> result_map) {
+
+
+
+    private void print_result(int min_distance, List result_list) {
         if (min_distance > 50) {
             println("places not found")
         } else {
 
-            println(result_map.keySet())
+            println(result_list[0])
         }
     }
 }
+//51.51453310076461, -0.1264620142104317
