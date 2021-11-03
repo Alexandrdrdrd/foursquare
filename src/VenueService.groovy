d8789465e5d3153e3e9dbf3cd750b9a57f0833cb
Alexandrdrdrd <kands12349@gmail.com>
2021-11-03 14:49:00 +0300

    request reduction

--- src/VenueService.groovy
+++ src/VenueService.groovy
@@ -10,19 +10,23 @@ class VenueService {
     public void find_nearest_venue(String[] coordinatesAndSection) throws IOException {
         try{
             JsonNode json_result = foursquareAPIhttpClient.findVenuesByDistance(coordinatesAndSection)
-            if (json_result.get("response").get("groups").get(0).get("items").size() == 0) {
+            println(json_result)
+            JsonNode item = json_result.get("response").get("groups").get(0).get("items")
+            if (item.size() == 0) {
                 println("There is no venues on current coordinates")
                 return
             }
-            int min_distance = json_result.get("response").get("groups").get(0).get("items").get(0).get("venue").get("location").get("distance").asInt()
+            int min_distance = item.get(0).get("venue").get("location").get("distance").asInt()
             def result_list = []
 
-            for (int i = 0; i<json_result.get("response").get("groups").get(0).get("items").size();i++){
+            for (int i = 0; i<item.size();i++){
                 Map<String, Integer> result_map = new HashMap<>()
-                result_map.put(json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("name").asText()
-                        , json_result.get("response").get("groups").get(0).get("items").get(i).get("venue").get("location").get("distance").asInt())
+                result_map.put(item.get(i).get("venue").get("name").asText()
+                        , item.get(i).get("venue").get("location").get("distance").asInt())
                 result_list.add(result_map)}
+            println(result_list)
             print_result(min_distance, result_list)
+
         }catch (IOException e){
             println(e.getMessage())
             println("Pleas, try again")
@@ -40,7 +44,7 @@ class VenueService {
         } else {
 
             println(result_list[0])
+            println(result_list)
         }
     }
 }
-//51.51453310076461, -0.1264620142104317
